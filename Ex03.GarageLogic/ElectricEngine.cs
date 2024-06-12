6b8fb98a50adeb28053a57111a23b2ec5e5218d2
Shayo <Shayo@SHAYO>
2024-06-12 17:14:00 +0300

    add class to add wheel pressure/electric/fuel

--- Ex03.GarageLogic/ElectricEngine.cs
+++ Ex03.GarageLogic/ElectricEngine.cs
@@ -4,9 +4,22 @@ using System.Collections.Generic;
 
 namespace Ex03.GarageLogic
 {
-    sealed class ElectricEngine
+    sealed class ElectricEngine : Engine
     {
         private float statusBattery; //hours for battery
         private float maximumForBattery; //how many hours can electric car drive by with maximum battery
+
+
+        protected override float getEnergy()
+        {
+            return statusBattery;
+        }
+
+
+        public void chargeBattery(int howManyHoursToAddToBattery)
+        {
+            statusBattery = WorkOnCar.addResourceToResource(statusBattery, howManyHoursToAddToBattery);
+        }
+
     }
 }
---
+++ Ex03.GarageLogic/WorkOnCar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex03.GarageLogic
+{
+    internal class WorkOnCar
+    {
+
+        public static int addResourceToResource(int addTo, int howMuchToAdd)
+        {
+            if (addTo + howMuchToAdd <= addTo)
+            {
+                addTo = addTo + howMuchToAdd;
+            }
+            //potentially need to throw exception
+            return addTo;
+        }
+
+        public static float addResourceToResource(float addTo, int howMuchToAdd)
+        {
+            if (addTo + howMuchToAdd <= addTo)
+            {
+                addTo = addTo + howMuchToAdd;
+            }
+            //potentially need to throw exception
+            return addTo;
+        }
+    }
+}
