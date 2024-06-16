18832f108c874cc487ca3f63c3dc1de18929f144
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2024-06-16 11:22:08 +0300

    Added back all the TODOs

--- Ex03.ConsoleUI/Program.cs
+++ Ex03.ConsoleUI/Program.cs
@@ -13,7 +13,7 @@ namespace Ex03.ConsoleUI
 
             while (userInput != "Q")
             {
-                Console.WriteLine("enter: insert(insert new vehicle), show all licenses, change vehicle status, add air pressure, add fuel to car, ");
+                Console.WriteLine("enter: insert(insert new vehicle), show all licenses, change vehicle status, add air pressure, add fuel to car, "); //TODO : fix message
                 userInput = Console.ReadLine();
                 try
                 {
@@ -72,6 +72,7 @@ namespace Ex03.ConsoleUI
             Console.WriteLine("enter the status you want for the car (INPROGRESS/FIXED/PAYED): ");
             string carStatus = Console.ReadLine();
 
+            // Send data basic about car to garage
             i_Garage.setLastEnteredVehicle(nameOfOwner);
             i_Garage.setLastEnteredVehicle(phoneOfOwner);
             i_Garage.setLastEnteredVehicle(carStatus);
@@ -97,6 +98,7 @@ namespace Ex03.ConsoleUI
                         if (!int.TryParse(inputAttribute, out int intInputAttribute))
                         {
                             throw new FormatException("Cannot convert to int");
+                            //TODO : exception cannot convert to int (done?)
                         }
                         i_Garage.setLastEnteredVehicle(intInputAttribute);
                         break;
@@ -104,6 +106,7 @@ namespace Ex03.ConsoleUI
                         if (!float.TryParse(inputAttribute, out float floatInputAttribute))
                         {
                             throw new FormatException("Cannot convert to float");
+                            //TODO : exception cannot convert to float (done?)
                         }
                         i_Garage.setLastEnteredVehicle(floatInputAttribute);
                         break;
@@ -111,10 +114,11 @@ namespace Ex03.ConsoleUI
                         if (!bool.TryParse(inputAttribute, out bool boolInputAttribute))
                         {
                             throw new FormatException("Cannot convert to bool");
+                            //TODO : exception cannot convert to boolean (done?)
                         }
                         i_Garage.setLastEnteredVehicle(boolInputAttribute);
                         break;
-                    default:
+                    default: // Needed to send string
                         i_Garage.setLastEnteredVehicle(inputAttribute);
                         break;
                 }
@@ -133,10 +137,20 @@ namespace Ex03.ConsoleUI
             Console.WriteLine("what type of licenses(INPROGRESS,FIXED,PAYED,ANY): ");
             string typeOfLicenses = Console.ReadLine();
 
-            List<string> allLicenses = i_Garage.showAllLicenses(typeOfLicenses);
-            foreach (string license in allLicenses)
+            switch (typeOfLicenses)
             {
-                Console.WriteLine(license);
+                case "A":
+                case "A1":
+                case "AA":
+                case "B1":
+                    List<string> allLicenses = i_Garage.showAllLicenses(typeOfLicenses); //TODO : need to handle exception (done?)
+                    foreach (string license in allLicenses)
+                    {
+                        Console.WriteLine(license);
+                    }
+                    break;
+                default:
+                    throw new ArgumentException($"No valid license type matches \"{typeOfLicenses}\"");
             }
         }
 
@@ -146,14 +160,14 @@ namespace Ex03.ConsoleUI
             string licenseCar = Console.ReadLine();
             Console.WriteLine("enter new status: ");
             string newStatus = Console.ReadLine();
-            i_Garage.changeStatusToCar(licenseCar, newStatus);
+            i_Garage.changeStatusToCar(licenseCar, newStatus); //TODO : need to handle exception
         }
 
         private static void addAirPressure(Garage i_Garage)
         {
             Console.WriteLine("enter license of car to add pressure to: ");
             string licenseCar = Console.ReadLine();
-            i_Garage.FillFullAirPressureInWheels(licenseCar);
+            i_Garage.FillFullAirPressureInWheels(licenseCar); //TODO : need to handle exception
         }
 
         private static void addFuel(Garage i_Garage)
@@ -165,10 +179,11 @@ namespace Ex03.ConsoleUI
             if (!float.TryParse(howMuchFuel, out float valueHowMuchFuel))
             {
                 throw new FormatException("Cannot convert to float");
+                //TODO : need to put this line in try
             }
             Console.WriteLine("enter what type of fuel do you want to use: ");
             string typeOfFuel = Console.ReadLine();
-            i_Garage.addFuel(licenseCar, valueHowMuchFuel, typeOfFuel);
+            i_Garage.addFuel(licenseCar, valueHowMuchFuel, typeOfFuel); //TODO : need to handle exception
         }
     }
 }
